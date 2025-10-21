4b61e1d974e468e4b83334f8edaac6d7c1583e0f
ByeonJuHwan <asdcz@naver.com>
2025-10-21 14:45:52 +0900

    예약 관련 주석추가

--- reservation-service/src/main/kotlin/org/ktor_lecture/reservationservice/application/service/ReservationService.kt
+++ reservation-service/src/main/kotlin/org/ktor_lecture/reservationservice/application/service/ReservationService.kt
@@ -4,15 +4,19 @@ import org.ktor_lecture.reservationservice.adapter.`in`.request.CreateReservatio
 import org.ktor_lecture.reservationservice.application.port.`in`.CreateReservationUseCase
 import org.ktor_lecture.reservationservice.application.port.out.ReservationRepository
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ReservationService (
     private val reservationRepository: ReservationRepository,
 ) : CreateReservationUseCase {
 
-    @Transactional
     override fun createReservation(userId: String, request: CreateReservationRequest) {
-        //
+        // 결제 먼저 진행 -> 결제 서비스에 요청
+
+        // 재고 감소 진행
+
+        // 예약 생성 -> 트랜잭션으로 만약 예약 실행이 안되면 이 범위만 트랜잭션 추가
+
+        // 여기서는 예약 완료 이벤트 발행 -> 알림
     }
 }
